00498aa7f95ed08df7c16f476a32cda5f898035f
yummycabbage <56630800+yummycabbage@users.noreply.github.com>
2020-02-09 21:44:34 +1000

    Base Project

---
+++ StratMakerR6/Assets/IconSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconSpawner : MonoBehaviour
+{
+
+    public GameObject icon;
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(icon, transform.position, transform.rotation);
+
+        }
+    }
+}
